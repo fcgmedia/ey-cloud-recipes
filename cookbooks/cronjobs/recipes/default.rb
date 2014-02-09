a6bef7dbdee7af3a76ea2ff6c1de92ad66ffa3fb
Samuel O. Obukwelu <sam@fcgmedia.com>
2014-02-09 07:24:36 -0500

    fixed missing variable

--- cookbooks/cronjobs/recipes/default.rb
+++ cookbooks/cronjobs/recipes/default.rb
@@ -6,12 +6,12 @@ ey_cloud_report "cronjobs" do
   message "setting this bad boy up"
 end
 
-node[:applications].each do |app_name, data|
+node[:applications].each do |app_name, _|
   if node[:name] == 'houston'
     cron "#{app_name}-maintenance" do
       minute '*/5'
       user 'deploy'
-      command "cd /data/#{appname}/current && RAILS_ENV=#{node[:environment][:name]} bin/rake maintenance:make_past_parties_inactive"
+      command "cd /data/#{app_name}/current && RAILS_ENV=#{node[:environment][:name]} bin/rake maintenance:make_past_parties_inactive"
     end
   end
 end
