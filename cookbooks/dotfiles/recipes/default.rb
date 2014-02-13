f9c91f9f365af55670c3de581f88d4428cff8244
Samuel O. Obukwelu <sam@fcgmedia.com>
2014-02-13 13:23:34 -0500

    created a dotfile

---
+++ cookbooks/dotfiles/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: dotfiles
+# Recipe:: default
+#
+
+if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
+  node[:applications].each do |app_name, data|
+    username = node[:users].first[:username]
+    template "/home/#{username}/.irbrc" do
+      source "irbrc.erb"
+      owner username
+      group username
+      mode 0744
+    end
+
+    template "/home/#{username}/.bashrc" do
+      source "irbrc.erb"
+      owner username
+      group username
+      mode 0744
+      variables({
+          :app_name => app_name
+      })
+    end
+  end
+end
